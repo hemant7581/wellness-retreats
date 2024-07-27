9126799e14ced1870de1ae8fb651f8bfaf03ec70
hemant <srivastavhemant17@gmail.com>
2024-07-28 01:48:56 +0530

    deleted redundant file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// import Header from './Components/Header';
+
 import Retreat from './Components/Reatreat';
 
 
 function App() {
   return (
     <div className='bg-[#fff]'>
-    {/* <Header/> */}
+ 
       <Retreat />
     </div>
   );
